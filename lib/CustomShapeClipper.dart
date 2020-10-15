a8bd24341f9c4f5d332c85a8fbfd103224080de7
Ayush mehta <ayushmehta651@gmail.com>
2020-10-15 20:12:51 +0530

    UI finished

--- lib/CustomShapeClipper.dart
+++ lib/CustomShapeClipper.dart
@@ -3,25 +3,25 @@ import 'package:flutter/material.dart';
 class CustomShapeClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    var firstEndPoint = Offset(size.width * 0.5, size.height - 30);
-    var firstControlPoint = Offset(size.width * 0.25, size.height - 50);
-
-    var secondEndPoint = Offset(size.width, size.height - 80);
-    var secondControlPoint = Offset(size.width * 0.75, size.height - 20);
+    var firstEndPoint = Offset(size.width * 0.55, size.height - 250);
+    var firstControlPoint = Offset(size.width * 0.40, size.height * 0.2);
+    var secondEndPoint = Offset(size.width, size.height * 0.40);
+    var secondControlPoint = Offset(size.width * 0.75, size.height * 0.99);
 
     Path path = Path()
-      ..lineTo(0.0, size.height)
+      ..lineTo(0, size.height)
       ..quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
           firstEndPoint.dx, firstEndPoint.dy)
       ..quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
           secondEndPoint.dx, secondEndPoint.dy)
       ..lineTo(size.width, 0)
       ..close();
+
     return path;
   }
 
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    return true;
+    return false;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:friendship_calculator/CustomShapeClipper.dart';
 
-void main() => runApp(MaterialApp(
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    ));
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [HomeScreenTopPart()],
-      ),
-    );
-  }
-}
-
-class HomeScreenTopPart extends StatefulWidget {
-  @override
-  _HomeScreenTopPartState createState() => _HomeScreenTopPartState();
-}
-
-class _HomeScreenTopPartState extends State<HomeScreenTopPart> {
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        ClipPath(
+      home: Scaffold(
+        body: Stack(children: <Widget>[
+          ClipPath(
             clipper: CustomShapeClipper(),
             child: Container(
-              height: 400.0,
-              decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      colors: [Color(0xFFEF772C), Color(0xFFEF772C)])),
-              child: Column(children: [
-                SizedBox(height: 40.0, width: 20.0),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Spacer(),
-                      Icon(
-                        Icons.settings,
-                        color: Colors.white,
-                      )
-                    ],
-                  ),
+                height: 600, decoration: BoxDecoration(color: Colors.orange[600])),
+          ),
+          Container(
+            child: Column(children: [
+              //SizedBox(height: 10.0),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 0.0),
+                  child: Image(
+                      height: 200,
+                      width: 250,
+                      image: AssetImage('assets/images/image1.jpg')),
                 ),
-                SizedBox(
-                  height: 100.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 32.0),
-                  child: Material(
-                    elevation: 5.0,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(30.0),
-                    ),
-                    child: TextField(
+              ),
+              //SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10,right: 50,left: 50,top: 10.0),
+                child: Material(
+                  elevation: 5.0,
+                  child: TextField(
                       controller:
                           TextEditingController(text: 'Enter 10 digits'),
-                      cursorColor: Colors.white,
                       decoration: InputDecoration(
                           contentPadding: EdgeInsets.symmetric(
-                              horizontal: 16.0, vertical: 13.0),
-                          border: InputBorder.none),
-                    ),
+                              horizontal: 16.0, vertical: 13.0))),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 230.0),
+                child: RaisedButton(
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
                   ),
-                )
-              ]),
-            )),
-      ],
+                  color: Colors.white,
+                  hoverColor: Colors.blue,
+                  child: Text('SEND OTP'),
+                ),
+              )
+            ]),
+          ),
+        ]),
+      ),
     );
   }
 }
